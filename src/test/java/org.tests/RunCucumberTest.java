7e0131c24fd188dd59658784b4db82ff8ce4fd5a
James-McAvilley <72215788+James-McAvilley@users.noreply.github.com>
2022-08-11 11:43:26 +0100

    Adding Cucumber

--- src/test/java/org.tests/RunCucumberTest.java
+++ src/test/java/org.tests/RunCucumberTest.java
@@ -4,7 +4,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-
+//New Change
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/cucumber",
                  glue = "src/test/java/org.tests/CatTest.java")
